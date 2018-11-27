99bd04ea6ee6e969a7830a19d6d6b283a74353e0
Jing Yang <jingyang_carl@qq.com>
2018-11-27 18:24:14 +0800

    31. initialization

---
+++ Lintcode/Solution_31.cpp
@@ -0,0 +1,7 @@
+#include "Solution_31.h"
+
+int Solution::partitionArray(vector<int>& nums, int k) {
+	// write your code here
+
+	return 0;
+}
---
+++ Lintcode/Solution_31.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	/**
+	 * @param nums: The integer array you should partition
+	 * @param k: An integer
+	 * @return: The index after partition
+	 */
+	int partitionArray(vector<int> &nums, int k);
+};
