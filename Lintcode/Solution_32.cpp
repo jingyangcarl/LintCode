9a985e0ff47e6bb49c1d1198fccad686d991ebdc
Jing Yang <jingyang_carl@qq.com>
2018-12-03 10:47:11 +0800

    32. try

--- Lintcode/Solution_32.cpp
+++ Lintcode/Solution_32.cpp
@@ -4,4 +4,24 @@ string Solution::minWindow(string & source, string & target) {
 	// write your code here
 
 	string::iterator srcIter, tempIter;
+	for (srcIter = source.begin(); srcIter != source.end() - target.size(); srcIter++) {
+
+		string temp = target;
+
+		for (tempIter = temp.begin(); tempIter != temp.end(); tempIter++) {
+			if (*srcIter == *tempIter) {
+				temp.erase(tempIter - target.begin());
+				break;
+			}
+		}
+
+
+		while (temp.size) {
+
+			for (tgtIter = target.begin(); tgtIter != target.end(); tgtIter++) {
+
+			}
+		}
+
+	}
 }
