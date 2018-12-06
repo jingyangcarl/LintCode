8b087c99fac075a36d63a5942be956ca67b2cb64
Jing Yang <jingyang_carl@qq.com>
2018-12-06 13:39:32 +0800

    35. Reverse Linked List PS: comment added

--- Lintcode/Solution_35.cpp
+++ Lintcode/Solution_35.cpp
@@ -32,10 +32,20 @@ void Solution_35::test() {
 ListNode * Solution_35::reverse(ListNode * head) {
 	// write your code here
 
+	// Carl: 
+	// C++ cannot return the pointer without the declaration of STATIC
+	// the runtime memory consists of heap and stack, where
+	// the heap lives with the main function
+	// the stack lives with its funtions
+	// if the nodeStack (stack<ListNode>) are not denoted as static, the variables will be collected when the funtion finished
+	// so as the rvsNode (ListNode),
+	// leading to the random result back to the calling function
+	// for details: see Onenote/Computer/CPP/QA - 函数能否返回局部指针变量
 	static ListNode *rvsNode = new ListNode(0);
 	static stack<ListNode> nodeStack;
 	ListNode *temp(NULL);
 
+	// Carl: push elements
 	temp = head;
 	while (temp) {
 		ListNode node = *temp;
@@ -44,6 +54,7 @@ ListNode * Solution_35::reverse(ListNode * head) {
 		temp = temp->next;
 	}
 
+	// Carl: reverse elements
 	temp = rvsNode;
 	while (!nodeStack.empty()) {
 		temp->next = &(nodeStack.top());
@@ -52,5 +63,6 @@ ListNode * Solution_35::reverse(ListNode * head) {
 	}
 	rvsNode = rvsNode->next;
 
+	// Carl: return
 	return rvsNode;
 }
