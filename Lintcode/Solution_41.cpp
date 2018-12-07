b6452f4e52afe6725a65d6e4a3f244ac2fb01ec1
Jing Yang <jingyang_carl@qq.com>
2018-12-07 10:59:19 +0800

    41. Maximum Subarray BUG1: error when 1000-size array

--- Lintcode/Solution_41.cpp
+++ Lintcode/Solution_41.cpp
@@ -1,60 +1,74 @@
 #include "Solution_41.h"
 
-int Solution_41::maxSubArray(vector<int> &nums) {
+int Solution_41::maxSubArray(vector<int>& nums) {
 	// write your code here
 
 	// Carl:
 	// start from the first element
-	// if the next element is positive or zero, add in the array
-	// if the next element is negative, the following procesures depend on the next positive elements until the next negative
-	//  if the absolute value of the on-coming negatives are larger than the sum of all on-coming positive elements, break the array, save and go on;
-	//  if the absolute value fo the on-coming negatives are smaller or equal than the sum of all on-comming positive elements, add the elements
+	// the array consists of only positives and negatives
+	// add all contiguous positives to posSum with initial value of 0
+	// add all contiguous negatives to negSum with initial value of 0
+	// compare posSum and negSum
+	// if posSum >= -negSum, add elements to the sum all the way to the end of negative elements
+	// else if posSum < -negSum, 
+	//  if posSum != 0, add all positive elements to the sum, save the value and restart the counting from the next positive element
+	//  else if posSum == 0, which means there is no positive value, then select the negatives with largest absolute value
 
 	int sum(0), sumTemp(0);
 	vector<int>::iterator iter, iterC;
 
-	// Carl: if the first element is negative, remove it
-	if (nums.at(0) < 0)
-		nums.erase(nums.begin());
+	// Carl: boundary detection, data sample: [-1] or [3]
+	if (nums.size() == 1) {
+		return *nums.begin();
+	}
 
 	for (iter = nums.begin(); iter != nums.end(); iter++) {
-		if (*iter >= 0) {
-			// Carl: if the next element is positive or equal to 0
-			sumTemp += *iter;
+		iterC = iter;
+
+		// Carl: all the contiguous positive elements
+		int posSum(0);
+		while (iterC != nums.end() && *iterC >= 0) {
+			posSum += *iterC;
+			iterC++;
 		}
-		else {
-			// Carl: if the next element is negative
-			iterC = iter;
+		// Carl: all the contiguous negative elements
+		int negSum(0);
+		while (iterC != nums.end() && *iterC < 0) {
+			negSum += *iterC;
+			iterC++;
+		}
 
-			// Carl: all the contiguous negative elements
-			int negSum(0);
-			while (*iterC < 0) {
-				negSum += *iterC;
-				iterC++;
-			}
-			// Carl: all the contiguous positive elements
-			int posSum(0);
-			while (*iterC >= 0) {
-				posSum += *iterC;
-				iterC++;
-			}
-
-			// Carl: compare
-			if (-negSum <= posSum) {
-				// Carl: add all the passed elements
+		// Carl: compare posSum and negSum
+		if (posSum != 0) {
+			if (posSum >= -negSum) {
 				for (iter; iter != iterC; iter++) {
 					sumTemp += *iter;
 				}
 			}
 			else {
-				// Carl: save and restart detection
+				while (*iter >= 0) {
+					sumTemp += *iter;
+					iter++;
+				}
 				sum = sumTemp > sum ? sumTemp : sum;
-				iter = iterC;
 				sumTemp = 0;
 			}
 		}
+		else {
+			int smallest(-65535);
+			for (iter; iter != nums.end() && *iter < 0; iter++) {
+				smallest = *iter > smallest ? *iter : smallest;
+			}
+			sum = smallest;
+			sumTemp = 0;
+		}
+		// Carl: iteration compensation
+		iter = --iterC;
 	}
 
+	// Carl: boundary detection, data sample [-1, 0, 1]
+	if(sumTemp > 0)
+		sum = sumTemp > sum ? sumTemp : sum;
 	return sum;
 }
 
