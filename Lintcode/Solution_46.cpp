133e8223581d50bef5279551ec28604891a64492
Jing Yang <jingyang_carl@qq.com>
2018-12-08 12:26:44 +0800

    46. try

---
+++ Lintcode/Solution_46.cpp
@@ -0,0 +1,43 @@
+#include "Solution_46.h"
+
+int Solution_46::majorityNumber(vector<int>& nums) {
+	// write your code here
+	
+	map<int, int> numMap;
+
+	for (vector<int>::iterator iter = nums.begin(); iter != nums.end(); iter++) {
+		if (numMap.find(*iter) == numMap.end()) {
+			numMap.insert(*iter, 1);
+		}
+		else {
+			(*numMap.find(*iter)).second++;
+		}
+	}
+	
+	int min(INT_MAX);
+	for (map<int, int>::iterator iter = numMap.begin(); iter != numMap.end(); iter++) {
+		if()
+	}
+
+	return 0;
+}
+
+void Solution_46::test() {
+	// write your test here
+
+	// Carl: input
+	int n;
+	cin >> n;
+	vector<int> nums;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		nums.push_back(num);
+	}
+
+	// Carl: algorithm
+	n = majorityNumber(nums);
+
+	// Carl: output
+	cout << n << endl;
+}
---
+++ Lintcode/Solution_46.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <map>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution_46 {
+public:
+	/*
+	 * @param nums: a list of integers
+	 * @return: find a  majority number
+	 */
+	int majorityNumber(vector<int> &nums);
+	void test();
+};
