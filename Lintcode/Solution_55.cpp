45790748a8a5076dc205ac70e3c6dfa2449e0712
Jing Yang <jingyang_carl@qq.com>
2018-12-09 15:49:38 +0800

    55. try

---
+++ Lintcode/Solution_55.cpp
@@ -0,0 +1,14 @@
+#include "Solution_55.h"
+
+bool Solution_55::compareStrings(string & A, string & B) {
+	// write your code here
+
+
+
+	return;
+}
+
+void Solution_55::test() {
+	// write your test here
+
+}
---
+++ Lintcode/Solution_55.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Solution_55 {
+public:
+	/**
+	 * @param A: A string
+	 * @param B: A string
+	 * @return: if string A contains all of the characters in B return true else return false
+	 */
+	bool compareStrings(string &A, string &B);
+	void test();
+};
