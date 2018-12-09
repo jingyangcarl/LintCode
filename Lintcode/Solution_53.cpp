4beb395e3ab1fe23323ba1a337b33e30fcbecfc0
Jing Yang <jingyang_carl@qq.com>
2018-12-09 10:58:08 +0800

    53. Reverse Words in a String PS: algorithm finished, a more clear idea is needed

--- Lintcode/Solution_53.cpp
+++ Lintcode/Solution_53.cpp
@@ -3,20 +3,39 @@
 string Solution_53::reverseWords(string & s) {
 	// write your code here
 
-	string::iterator iter, iterWord;
+	// Carl:
+	// Idea:
+	// record the start of the word and the start of the spaces
+	// the characters in between are a word
+
+	string::iterator iter, iterWord(s.end());
+	bool saveTag(0);
 	stack<string> strStack;
 
 	for (iter = s.begin(); iter != s.end(); iter++) {
-		if (*iter != ' ') {
+		if (saveTag == 0 && *iter != ' ') {
 			iterWord = iter;
+			saveTag = 1;
 		}
-		else {
+		else if (saveTag == 1 && *iter == ' ') {
 			string word = s.substr(iterWord - s.begin(), iter - iterWord);
 			if (word.size()) strStack.push(word);
+			saveTag = 0;
 		}
 	}
+	if (saveTag == 1 && iterWord != s.end()) {
+		// boundary detection 
+		string word = s.substr(iterWord - s.begin(), iter - iterWord);
+		if (word.size()) strStack.push(word);
+	}
 
-	return 0;
+	string result;
+	while (!strStack.empty()) {
+		result += strStack.top() + " ";
+		strStack.pop();
+	}
+
+	return result;
 }
 
 void Solution_53::test() {
@@ -24,7 +43,7 @@ void Solution_53::test() {
 
 	// Carl: input
 	string s, r;
-	cin >> s;
+	getline(cin, s);
 
 	// Carl: algorithm
 	r = reverseWords(s);
--- Lintcode/main.cpp
+++ Lintcode/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include "Solution_50.h"
+#include "Solution_53.h"
 using namespace std;
 
 int main() {
 
-	Solution_50 solution;
+	Solution_53 solution;
 	solution.test();
 
 	getchar();
