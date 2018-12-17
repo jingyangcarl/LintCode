d7078674009744a4252c2bf78d7c1f4cc0bb4cd0
Jing Yang <jingyang_carl@qq.com>
2018-12-17 19:10:58 +0800

    93. Balanced Binary Tree PS: recursion

--- Lintcode/Solution_93.cpp
+++ Lintcode/Solution_93.cpp
@@ -2,17 +2,15 @@
 
 bool Solution_93::isBalanced(TreeNode * root) {
 	// write your code here
-	int leftHeight(0), rightHeight(0);
-	bool is(true);
-
-	return is;
+	if (!root) return true;
+	if (abs(depth(root->left) - depth(root->right)) > 1) return false;
+	return isBalanced(root->left) && isBalanced(root->right);
 }
 
 int Solution_93::depth(TreeNode * root) {
+	// write your code here
 	if (!root) return 0;
-	else {
-
-	}
+	else return 1 + max(depth(root->left), depth(root->right));
 }
 
 void Solution_93::create(TreeNode *& node) {
