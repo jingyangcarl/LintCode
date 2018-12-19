54b936d6a402916a2a6aa993712f35bd55518b24
Jing Yang <jingyang_carl@qq.com>
2018-12-19 09:29:47 +0800

    110. try divide and conquer

--- Lintcode/Solution_110.cpp
+++ Lintcode/Solution_110.cpp
@@ -2,9 +2,12 @@
 
 int Solution_110::minPathSum(vector<vector<int>>& grid) {
 	// write your code here
+	// try dynamic programming
+	// try divide and conquer
 	return 0;
 }
 
 void Solution_110::test() {
 	// write your code here
+	// try 
 }
