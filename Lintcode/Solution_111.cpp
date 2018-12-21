7622690f4cd1f22de508a485d610f2ef737f6dc8
Jing Yang <jingyang_carl@qq.com>
2018-12-21 09:51:26 +0800

    111. try

---
+++ Lintcode/Solution_111.cpp
@@ -0,0 +1,6 @@
+#include "Solution_111.h"
+
+int Solution_111::climbStairs(int n) {
+	// write your code here
+	return 0;
+}
---
+++ Lintcode/Solution_111.h
@@ -0,0 +1,9 @@
+#pragma once
+class Solution_111 {
+public:
+	/**
+	 * @param n: An integer
+	 * @return: An integer
+	 */
+	int climbStairs(int n);
+};
