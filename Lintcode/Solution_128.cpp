4a9117fb5c98dd99733c3ed08e06291edb81ec4b
Jing Yang <jingyang_carl@qq.com>
2018-12-30 22:30:56 -0800

    128. Hash Function

--- Lintcode/Solution_128.cpp
+++ Lintcode/Solution_128.cpp
@@ -20,7 +20,25 @@ int Solution_128::hashCode(string & key, int HASH_SIZE) {
 }
 
 int Solution_128::hashCode_2(string & key, int HASH_SIZE) {
-	return 0;
+	// write your code here
+
+	// Carl: 
+	// Idea: 
+	// if the string is too long, the accumulation of the product will be increated in a power level
+	// which will lead to the overflow of an int or evel a longlong
+	// however, the digits larger than HASH_SIZE will not be counted in the calculation
+	// hence, do the module every time when calculate the powers
+
+	long long sum(0);
+	vector<long long> factor(key.size(), 1);
+
+	for (int i = 1; i < key.size(); i++)
+		factor[i] = factor[i - 1] * 33 % HASH_SIZE;
+
+	for (int i = 0; i < key.size(); i++)
+		sum += factor[i] * (int)key[-i + key.size() - 1];
+
+	return sum % HASH_SIZE;
 }
 
 void Solution_128::test() {
@@ -33,5 +51,5 @@ void Solution_128::test() {
 	cin >> HASH_SIZE;
 
 	// algorithm and output
-	cout << hashCode(key, HASH_SIZE);
+	cout << hashCode_2(key, HASH_SIZE);
 }
