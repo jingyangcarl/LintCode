cb0764b502ed386a1153d43a5680209f5c9f1ac6
Jing Yang <jingyang_carl@qq.com>
2019-01-05 20:37:30 -0800

    142. try

---
+++ Lintcode/Solution_142.cpp
@@ -0,0 +1,17 @@
+#include "Solution_142.h"
+
+bool Solution::checkPowerOf2(int n) {
+	// write your code here
+	return false;
+}
+
+void Solution::test() {
+	// write your test here
+
+	// input;
+	int n;
+	cin >> n;
+
+	// algorithm and output
+
+}
---
+++ Lintcode/Solution_142.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	/**
+	 * @param n: An integer
+	 * @return: True or false
+	 */
+	bool checkPowerOf2(int n);
+	void test();
+};
