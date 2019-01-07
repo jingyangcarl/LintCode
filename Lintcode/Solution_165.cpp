1bc6f247d3a0e1ee23a3f2a618e642723e00b522
Jing Yang <jingyang_carl@qq.com>
2019-01-07 11:28:03 -0800

    165. try

---
+++ Lintcode/Solution_165.cpp
@@ -0,0 +1,6 @@
+#include "Solution_165.h"
+
+ListNode * Solution_165::mergeTwoLists(ListNode * l1, ListNode * l2) {
+	// write your code here
+	return nullptr;
+}
---
+++ Lintcode/Solution_165.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+// Definition of singly-linked-list:
+class ListNode {
+public:
+	int val;
+	ListNode *next;
+	ListNode(int val) {
+		this->val = val;
+		this->next = NULL;
+	}
+};
+
+class Solution_165 {
+public:
+	/**
+	 * @param l1: ListNode l1 is the head of the linked list
+	 * @param l2: ListNode l2 is the head of the linked list
+	 * @return: ListNode head of linked list
+	 */
+	ListNode * mergeTwoLists(ListNode * l1, ListNode * l2);
+};
