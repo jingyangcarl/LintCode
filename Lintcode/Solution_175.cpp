f8d3e66a409e16eaad4f7c2768ed92348ea40ab0
Jing Yang <jingyang_carl@qq.com>
2019-01-10 10:44:51 -0800

    175. try recursion

---
+++ Lintcode/Solution_175.cpp
@@ -0,0 +1,43 @@
+#include "Solution_175.h"
+
+void Solution_175::invertBinaryTree(TreeNode * root) {
+	// write your code here
+	if (root) {
+		TreeNode * temp = root->left;
+		root->left = root->right;
+		root->right = temp;
+	}
+}
+
+void Solution_175::create(TreeNode *& root) {
+	int num;
+	cin >> num;
+	if (num == -1) root = NULL;
+	else {
+		root = new TreeNode(num);
+		create(root->left);
+		create(root->right);
+	}
+}
+
+void Solution_175::preorderTraversal(TreeNode * root) {
+	if (root) {
+		cout << root->val << ' ';
+		preorderTraversal(root->left);
+		preorderTraversal(root->right);
+	}
+}
+
+void Solution_175::test() {
+	// write your test here
+
+	// input
+	TreeNode *root;
+	create(root);
+
+	// algorithm;
+	invertBinaryTree(root);
+
+	// output;
+	preorderTraversal(root);
+}
---
+++ Lintcode/Solution_175.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+// Definition of TreeNode:
+class TreeNode {
+public:
+	int val;
+	TreeNode *left, *right;
+	TreeNode(int val) {
+		this->val = val;
+		this->left = this->right = NULL;
+	}
+};
+
+class Solution_175 {
+public:
+	/**
+	 * @param root: a TreeNode, the root of the binary tree
+	 * @return: nothing
+	 */
+	void invertBinaryTree(TreeNode * root);
+	void create(TreeNode * &root);
+	void preorderTraversal(TreeNode *root);
+	void test();
+};
