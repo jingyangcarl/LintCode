080d8a1b6b7096515e137d1fb377a0647cf03d77
Jing Yang <jingyang_carl@qq.com>
2019-01-13 11:35:42 -0800

    204. try

---
+++ Lintcode/Solution_204.cpp
@@ -0,0 +1,6 @@
+#include "Solution_204.h"
+
+Solution_204 * Solution_204::getInstance() {
+	// write your code here
+	return nullptr;
+}
---
+++ Lintcode/Solution_204.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Solution_204 {
+public:
+	/**
+	 * @return: The same instance of this class every time
+	 */
+	static Solution_204* getInstance();
+};
