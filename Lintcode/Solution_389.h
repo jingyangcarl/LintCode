d3935abbc5b482938643aa9c9cce5ed1643ee01a
Jing Yang <jingyang_carl@qq.com>
2019-01-17 10:10:57 -0800

    389. search

---
+++ Lintcode/Solution_389.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	/**
+	 * @param board: the board
+	 * @return: whether the Sudoku is valid
+	 */
+	bool isValidSudoku(vector<vector<char>> &board) {
+		// write your code here
+	}
+};
