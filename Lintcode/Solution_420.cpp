c8ba5113efbff2ad2c885845c7eb23a29d2ec4c2
Jing Yang <jingyang_carl@qq.com>
2019-01-19 12:15:38 -0800

    420. try

---
+++ Lintcode/Solution_420.cpp
@@ -0,0 +1,17 @@
+#include "Solution_420.h"
+
+string Solution::countAndSay(int n) {
+	// write your code here
+	return string();
+}
+
+void Solution::test() {
+	// write your test here
+
+	// intput
+	int n;
+	cin >> n;
+
+	// algorithm and output
+	cout << countAndSay(n);
+}
---
+++ Lintcode/Solution_420.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	/**
+	 * @param n: the nth
+	 * @return: the nth sequence
+	 */
+	string countAndSay(int n);
+	void test();
+};
