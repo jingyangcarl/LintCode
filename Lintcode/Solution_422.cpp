a25da6c3aebdd283d5119d9a52f28c36a2d9ca9d
Jing Yang <jingyang_carl@qq.com>
2019-01-20 15:39:03 -0800

    422. Length of Last Word PS: Loop Rank: 93.40%

---
+++ Lintcode/Solution_422.cpp
@@ -0,0 +1,24 @@
+#include "Solution_422.h"
+
+int Solution::lengthOfLastWord(string & s) {
+	// write your code here
+
+	int lastLength(0);
+	for (int i = s.size() - 1; i >= 0; i--) {
+		if (s[i] != ' ') lastLength++;
+		else if (lastLength != 0) return lastLength;
+	}
+
+	return lastLength;
+}
+
+void Solution::test() {
+	// write your test here
+
+	// input
+	string str;
+	cin >> str;
+
+	// algorithm and output
+	cout << lengthOfLastWord(str) << endl;
+}
---
+++ Lintcode/Solution_422.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	/**
+	 * @param s: A string
+	 * @return: the length of last word
+	 */
+	int lengthOfLastWord(string &s);
+	void test();
+};
