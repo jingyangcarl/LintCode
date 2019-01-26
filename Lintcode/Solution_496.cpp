f385213aff20aa20da95aca559c69f7033856d45
Jing Yang <jingyang_carl@qq.com>
2019-01-26 10:29:39 -0800

    496. vitural function

---
+++ Lintcode/Solution_496.cpp
@@ -0,0 +1,7 @@
+#include "Solution_496.h"
+
+void Solution_496::test() {
+
+	ToyFactory factory = ToyFactory();
+	Toy toy = factory.getToy('Dog');
+}
---
+++ Lintcode/Solution_496.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+/**
+ * Your object will be instantiated and called as such:
+ * ToyFactory* tf = new ToyFactory();
+ * Toy* toy = tf->getToy(type);
+ * toy->talk();
+ */
+class Toy {
+public:
+	virtual void talk() const = 0;
+};
+
+class Dog : public Toy {
+	// Write your code here
+	void talk() {
+
+	}
+};
+
+class Cat : public Toy {
+	// Write your code here
+	void talk() {
+
+	}
+};
+
+class ToyFactory {
+public:
+	/**
+	 * @param type a string
+	 * @return Get object of the type
+	 */
+	Toy* getToy(string& type) {
+		// Write your code here
+		if (type == "Dog") {
+			return Dog();
+		}
+		else if (type == "Cat") {
+
+		}
+	}
+
+};
+
+class Solution_496 {
+public:
+	//ToyFactory factory;
+
+	void test();
+};
