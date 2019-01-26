b4873e21c32ce09900154842d5d25415558889f9
Jing Yang <jingyang_carl@qq.com>
2019-01-26 09:25:40 -0800

    try unordered_set

---
+++ Lintcode/Solution_488.cpp
@@ -0,0 +1,20 @@
+#include "Solution_488.h"
+
+bool Solution::isHappy(int n) {
+	// write your code here
+
+
+
+	return false;
+}
+
+void Solution::test() {
+	// write your test here
+
+	// input
+	int n;
+	cin >> n;
+
+	// algorithm and output
+	cout << isHappy(n);
+}
---
+++ Lintcode/Solution_488.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+	/**
+	 * @param n: An integer
+	 * @return: true if this is a happy number or false
+	 */
+	bool isHappy(int n);
+	void test();
+};
